d80f233e56f1a4aa11429b5b266cf8b1fdd5fd8e
Inaki Villar <inaki.seri@gmail.com>
2025-03-14 17:43:17 -0700

    fixing input incorrect companion function

--- plugin/src/main/kotlin/io/github/cdsap/inputsimporter/InputsImporter.kt
+++ plugin/src/main/kotlin/io/github/cdsap/inputsimporter/InputsImporter.kt
@@ -6,12 +6,11 @@ import org.gradle.api.tasks.ClasspathNormalizer
 import org.gradle.kotlin.dsl.named
 
 class InputsImporter {
-    companion object  {
+    companion object {
         fun add(
             target: Project,
             nameTask: String,
-            task: Task,
-            includeTestClasses: Boolean = false
+            task: Task
         ) {
             task.inputs.dir(
                 target.tasks.named<ImportReporterWIthInputsTask>(nameTask)
@@ -28,17 +27,37 @@ class InputsImporter {
                     .flatMap { it.compileRuntimeApp })
                 .withPropertyName("compile-runtime-app")
                 .withNormalizer(ClasspathNormalizer::class.java)
-            if (includeTestClasses) {
-                task.inputs.dir(
-                    target.tasks.named<ImportReporterWIthInputsTask>(nameTask)
-                        .flatMap { it.kotlinClasses }).withPropertyName("kotlin-classes")
-                    .withNormalizer(ClasspathNormalizer::class.java)
-                task.inputs.dir(
-                    target.tasks.named<ImportReporterWIthInputsTask>(nameTask)
-                        .flatMap { it.testJavaClasses }).withPropertyName("java-classes")
-                    .withNormalizer(ClasspathNormalizer::class.java)
-            }
         }
 
+        fun addWithClasses(
+            target: Project,
+            nameTask: String,
+            task: Task
+        ) {
+            task.inputs.dir(
+                target.tasks.named<ImportReporterWIthInputsTask>(nameTask)
+                    .flatMap { it.compileDependencies })
+                .withPropertyName("compile-dependencies")
+                .withNormalizer(ClasspathNormalizer::class.java)
+            task.inputs.dir(
+                target.tasks.named<ImportReporterWIthInputsTask>(nameTask)
+                    .flatMap { it.compileApp })
+                .withPropertyName("compile-app")
+                .withNormalizer(ClasspathNormalizer::class.java)
+            task.inputs.dir(
+                target.tasks.named<ImportReporterWIthInputsTask>(nameTask)
+                    .flatMap { it.compileRuntimeApp })
+                .withPropertyName("compile-runtime-app")
+                .withNormalizer(ClasspathNormalizer::class.java)
+            task.inputs.dir(
+                target.tasks.named<ImportReporterWIthInputsTask>(nameTask)
+                    .flatMap { it.kotlinClasses }).withPropertyName("kotlin-classes")
+                .withNormalizer(ClasspathNormalizer::class.java)
+            task.inputs.dir(
+                target.tasks.named<ImportReporterWIthInputsTask>(nameTask)
+                    .flatMap { it.testJavaClasses }).withPropertyName("java-classes")
+                .withNormalizer(ClasspathNormalizer::class.java)
+
+        }
     }
 }
